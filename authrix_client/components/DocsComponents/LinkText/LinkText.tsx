733f8d53ed5e8e20ae3da8d6c73ccf7b7ce0f8b7
separatrix <nikitalochmatov@gmail.com>
2025-05-12 11:27:01 +0300

    fix client link

--- authrix_client/components/DocsComponents/LinkText/LinkText.tsx
+++ authrix_client/components/DocsComponents/LinkText/LinkText.tsx
@@ -1,11 +1,14 @@
 import { LinkTextProps } from './LinkText.props';
 import styles from './LinkText.module.css';
 import Link from 'next/link';
+import { useLocale } from 'next-intl';
 
 
 export default function LinkText({ link, text }: LinkTextProps) {
+    const locale = useLocale();
+    
     return (
-        <Link href={link} className={styles.linkText}>
+        <Link href={link} className={styles.linkText} locale={locale}>
             {text}
         </Link>
     );
