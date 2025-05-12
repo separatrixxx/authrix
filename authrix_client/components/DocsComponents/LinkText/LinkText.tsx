0cd47b4ed6d6206b0358a5e24eb1da2334515e2f
separatrix <nikitalochmatov@gmail.com>
2025-05-12 11:36:31 +0300

    fix 3

--- authrix_client/components/DocsComponents/LinkText/LinkText.tsx
+++ authrix_client/components/DocsComponents/LinkText/LinkText.tsx
@@ -1,12 +1,13 @@
-'use client'
 import { LinkTextProps } from './LinkText.props';
 import styles from './LinkText.module.css';
 import Link from 'next/link';
 
 
 export default function LinkText({ link, text }: LinkTextProps) {    
+    const isTargetBlank = link.includes('.zip');
+
     return (
-        <Link href={link} className={styles.linkText}>
+        <Link href={link} className={styles.linkText} target={isTargetBlank ? '_blank' : ''}>
             {text}
         </Link>
     );
